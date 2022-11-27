4822f1595373af4a151782fa60c23a4e22773644
elamin.fadlalla <elamin.fadlalla@capgemini.com>
2022-11-27 14:13:35 +0000

    them setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
+import { ThemeProvider } from "@material-ui/styles";
 import Header from "./ui/Header";
+import theme from './ui/Theme';
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
       <Header />
       Hello!
-    </div>
+    </ThemeProvider>
   );
 }
 
